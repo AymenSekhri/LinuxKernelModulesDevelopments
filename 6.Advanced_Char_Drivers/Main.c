d6d89d5e50c42ee2c15c65845730acc5fa68cbcc
AymenSekhri <aymen.sekhri@live.fr>
2020-03-22 22:29:02 +0100

    fixing the crash in wake_up_interruptible
    The crash was because device_info structure has been copied to the stack not used directly from the global variable.

--- 6.Advanced_Char_Drivers/Main.c
+++ 6.Advanced_Char_Drivers/Main.c
@@ -8,7 +8,7 @@
 #include <linux/uaccess.h>
 #include <linux/version.h>
 #include <linux/wait.h>
-
+ 
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("Aymen Sekhri");
 MODULE_DESCRIPTION("A Simple Hello World module");
@@ -21,20 +21,20 @@ struct device_info {
     struct mutex my_mutex;
     struct wait_queue_head my_queue;
 };
-
+ 
 struct device_info* charDevices;
-
+ 
 #define DEVICE_BLOCK_SIZE 1000
-
+ 
 static void device_info_cleanup(void);
 int setup_cdevice(int index);
-
+ 
 int device_open(struct inode* myinode, struct file* fd)
 {
     printk(KERN_INFO "MyLinuxModule: device has been opened,     fd=%X.\n", fd);
     return 0;
 }
-
+ 
 int device_release(struct inode* myinode, struct file* fd)
 {
     printk(KERN_INFO "MyLinuxModule: device has been released,   fd=%X.\n", fd);
@@ -44,49 +44,47 @@ ssize_t device_read(struct file* fd, char __user* buf, size_t size,
     loff_t* offset)
 {
     printk(KERN_INFO "MyLinuxModule: device is being read from,  fd=%X.\n", fd);
-    struct device_info mydevice = charDevices[MINOR(fd->f_inode->i_rdev)];
-    if (mutex_lock_interruptible(&mydevice.my_mutex))
+    struct device_info *mydevice = &charDevices[MINOR(fd->f_inode->i_rdev)];
+    if (mutex_lock_interruptible(&mydevice->my_mutex))
         return -ERESTARTSYS;
-    while (*offset >= mydevice.max_data_avaliable) {
-		printk(KERN_INFO "MyLinuxModule: device is blocking...,      fd=%X.\n", fd);
-        mutex_unlock(&mydevice.my_mutex);
+    while (*offset >= mydevice->max_data_avaliable) {
+        printk(KERN_INFO "MyLinuxModule: driver is blocking...,      fd=%X.\n", fd);
+        mutex_unlock(&mydevice->my_mutex);
         /*if (fd->f_flags & O_NONBLOCK) // Non Blocking IO
             return -EAGAIN;*/
-        if (wait_event_interruptible(mydevice.my_queue,
-                *offset < mydevice.max_data_avaliable))
+        if (wait_event_interruptible(mydevice->my_queue,
+                *offset < mydevice->max_data_avaliable))
             return -ERESTARTSYS;
-        if (mutex_lock_interruptible(&mydevice.my_mutex))
+        if (mutex_lock_interruptible(&mydevice->my_mutex))
             return -ERESTARTSYS;
     }
-    int true_len = min(size, mydevice.max_data_avaliable - *offset);
-    printk(KERN_INFO "MyLinuxModule: true_len=%d, size=%d, left=%d fd=%X.\n",size, mydevice.max_data_avaliable - *offset, fd);
-    copy_to_user(buf, mydevice.data, true_len);
-    mutex_unlock(&mydevice.my_mutex);
+    int true_len = min(size, mydevice->max_data_avaliable - *offset);
+    copy_to_user(buf, mydevice->data, true_len);
+    mutex_unlock(&mydevice->my_mutex);
     *offset += true_len;
     return true_len;
 }
-
+ 
 ssize_t device_write(struct file* fd, const char __user* buf, size_t size,
     loff_t* offset)
 {
     printk(KERN_INFO "MyLinuxModule: device is being written to, fd=%X.\n", fd);
-    struct device_info mydevice = charDevices[MINOR(fd->f_inode->i_rdev)];
+    struct device_info *mydevice = &charDevices[MINOR(fd->f_inode->i_rdev)];
     if (*offset > DEVICE_BLOCK_SIZE) // reached Max size
     {
         return 0;
     }
     int true_len = min(size, DEVICE_BLOCK_SIZE - *offset);
-    if (mutex_lock_interruptible(&mydevice.my_mutex))
+    if (mutex_lock_interruptible(&mydevice->my_mutex))
         return -ERESTARTSYS;
-    copy_from_user(mydevice.data, buf, true_len);
-    mydevice.max_data_avaliable += true_len;
-    mutex_unlock(&mydevice.my_mutex);
-	printk(KERN_INFO "MyLinuxModule: my_queue=%X.\n", &mydevice.my_queue);
-	wake_up_interruptible(&mydevice.my_queue);
+    copy_from_user(mydevice->data + *offset, buf, true_len);
+    mydevice->max_data_avaliable += true_len;
+    mutex_unlock(&mydevice->my_mutex);
+    wake_up_interruptible(&mydevice->my_queue);
     *offset += true_len;
     return true_len;
 }
-
+ 
 struct file_operations myfops = { .owner = THIS_MODULE,
     .read = device_read,
     .write = device_write,
@@ -94,7 +92,7 @@ struct file_operations myfops = { .owner = THIS_MODULE,
     .release = device_release,
     .llseek = 0,
     .unlocked_ioctl = 0 };
-
+ 
 static int __init hello_init(void)
 {
     charDevices = kmalloc(sizeof(*charDevices) * numOfDevices, GFP_KERNEL);
@@ -111,7 +109,7 @@ static int __init hello_init(void)
         printk(KERN_INFO "MyLinuxModule: alloc_chrdev_region Error : %X\n", err);
         goto freeingMem;
     }
-
+ 
     for (size_t i = 0; i < numOfDevices; i++) {
         if (setup_cdevice(i)) {
             device_info_cleanup();
@@ -119,7 +117,7 @@ static int __init hello_init(void)
             goto freeingMem;
         }
     }
-
+ 
     return 0; // Non-zero return means that the module couldn't be loaded.
 freeingMem:
     kfree(charDevices);
@@ -134,12 +132,11 @@ static void hello_cleanup(void)
 }
 int setup_cdevice(int index)
 {
-    char* device_message = "Hello from the other side\n";
     // initiate the cdev struct
     cdev_init(&charDevices[index].chardev, &myfops);
     // initiate the data
     charDevices[index].data = kmalloc(DEVICE_BLOCK_SIZE, GFP_KERNEL);
-    memcpy(charDevices[index].data, device_message, strlen(device_message));
+    memset(charDevices[index].data,0 , DEVICE_BLOCK_SIZE);
     // initiate the semaphore & queue
     mutex_init(&charDevices[index].my_mutex);
     init_waitqueue_head(&charDevices[index].my_queue);
@@ -163,8 +160,8 @@ static void device_info_cleanup()
         }
     }
 }
-
-
-
+ 
+ 
+ 
 module_init(hello_init);
 module_exit(hello_cleanup);
